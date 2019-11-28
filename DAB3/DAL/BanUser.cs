1d7e43bff00d2a8b3cf1aa63e6416ce244507a72
Nicolai <nicolai@nicolaifisker.dk>
2019-11-28 12:31:58 +0100

    Ban

---
+++ DAB3/DAL/BanUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using DAB3.Models;
+using DAB3.Services;
+using MongoDB.Driver;
+
+namespace DAB3.DAL
+{
+    public class BanUser
+    {
+        
+
+        public void Banvser(string id)
+        {
+            UsersService _usersService =new UsersService();
+
+
+
+
+        }
+
+    }
+
+    
+}
